f0e149911e08eee17685f323835b010388fbadd5
Carlos <carlostarrazona7@gmail.com>
2020-11-21 00:03:12 +0100

    Repositori amb practiques objectes

--- app/act-POO/p2/scripts/activitat2.js
+++ app/act-POO/p2/scripts/activitat2.js
@@ -2,7 +2,7 @@
  * Muestra el resultado de la media de todos los valores del array
  */
 
-Array.prototype.media = function () {
+Array.prototype.sacarMedia = function () {
   return this.reduce((acu, valor) => acu + valor) / this.length;
 };
 
@@ -10,10 +10,10 @@ array1 = [1, 2, 3, 4, 5];
 
 console.log(`Array1: ${array1}`);
 
-console.log('Media array1: ' + array1.media());
+console.log('Media array1: ' + array1.sacarMedia());
 
-array2 = [10, 5, 6, 7, 9];
+array2 = [9, 2, 9, 1, 6];
 
 console.log(`Array2: ${array2}`);
 
-console.log('Media array2: ' + array2.media());
+console.log('Media array2: ' + array2.sacarMedia());
